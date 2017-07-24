afc852d2da97e7faf8f5f493c6d4ddb6abdc133b
dennismacbookpro <lawjolla@aol.com>
2017-07-23 21:44:23 -0700

    Fake store data model

--- src/components/victoryLineGraph.js
+++ src/components/victoryLineGraph.js
@@ -1,38 +1,84 @@
-import React, { Component } from 'react';
-import { VictoryLine, VictoryChart, VictoryTheme } from 'victory';
+import React, { Component } from "react";
+import { VictoryAxis, VictoryLine, VictoryChart, VictoryTheme, VictoryTooltip } from "victory";
 
 class LineGraph extends Component {
-  state = { width: 800 };
+  state = { width: 800, height: 400, parsedData: [] };
+
   parseLine(data, x, y) {
-    return data.map((sample, index) => {
-      if (sample[x] === '' || sample[y[0]] === '') return undefined;
-      return {
-        x: parseFloat(sample[x]),
-        y: parseFloat(sample[y[0]])
-      };
-    }).filter(item => item);
+    console.log("parsing...");
+    return data
+      .map((sample, index) => {
+        if (sample[x] === "" || sample[y[0]] === "") return undefined;
+        return {
+          x: parseFloat(sample[x]),
+          y: parseFloat(sample[y[0]]),
+          label: parseFloat(sample[y[0]])
+        };
+      })
+      .filter(item => item);
+  }
+  componentWillReceiveProps(props) {
+    this.updateData(props);
   }
   componentDidMount() {
-    const el = document.getElementById('hi');
-    this.setState({ width: el.offsetWidth })
-    window.addEventListener('resize', () => {
-      const el = document.getElementById('hi');
-      this.setState({ width: el.offsetWidth })
-    })
+    this.resizeGraph();
+    window.addEventListener("resize", () => this.resizeGraph());
   }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.resizeGraph());
+  }
+  resizeGraph = () => {
+    const smallestHeight = 300;
+    const smallestWidth = 400;
+    const { offsetHeight, offsetWidth } = this.chart;
+    const height =
+      offsetHeight < smallestHeight ? smallestHeight : offsetHeight;
+    const width = offsetWidth < smallestWidth ? smallestWidth : offsetWidth;
+    this.setState({ height, width });
+  };
+  updateData = ({ data, xText, yText }) => {
+    const parsedData = this.parseLine(data, xText, yText);
+    this.setState({ parsedData });
+  };
   render() {
-    const { x, y, data, xLine, yLines } = this.props;
-    const parsedData = this.parseLine(data, xLine, yLines);
+    const { parsedData, width, height } = this.state;
+    const { xText, yText } = this.props;
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100vh'}}>
-          <div style={{ flex: '0 0 50%', overflow: 'hidden'}} id="hi">
-            <VictoryChart width={this.state.width}>
-              <VictoryLine data={parsedData}/>
-            </VictoryChart>
-          </div>
-          <div  style={{ flex: '0 0 50%'}}>Hi</div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          height: "100vh"
+        }}
+      >
+        <div
+          style={{ flex: "0 0 50%", overflow: "hidden" }}
+          ref={ref => {
+            this.chart = ref;
+          }}
+        >
+          <VictoryChart
+            width={width}
+            height={height}
+            theme={VictoryTheme.material}
+            title="hi"
+          >
+            <VictoryAxis
+                style={{ parent: { border: "1px solid #ccc" } }}
+                label={xText}
+            />
+            <VictoryAxis dependentAxis
+                style={{ parent: { border: "1px solid #ccc" } }}
+                label={yText[0]}
+            />
+
+            <VictoryLine data={parsedData} labelComponent={<VictoryTooltip/>} />
+          </VictoryChart>
         </div>
-    )
+        <div style={{ flex: "0 0 50%" }}>Hi</div>
+      </div>
+    );
   }
 }
 
--- src/container/tempCSVContainer.js
+++ src/container/tempCSVContainer.js
@@ -5,35 +5,36 @@ const electron = window.require('electron');
 const { ipcRenderer }  = electron;
 
 class TempContainer extends Component {
-  state = { data: [], x: 0, y: 1, xLine: 'Time (sec)', yLines: ['ECU: MAP (PSI)'] };
+  state = { data: [], x: 0, y: 1, xText: 'ECU: RPM (RPM)', yText: ['ECU: MAP (PSI)'] };
 
   componentWillMount() {
     this.ipc = ipcRenderer.on('csv:open', (e, d) => {
       const data = ParseCSV(d).data;
-      console.log(data);
       const { x, y } = this.defaultAxis(data);
       this.setState({ data, x, y });
     });
     document.addEventListener("keydown", this.handleKeyDown);
   }
   defaultAxis = (data) => {
-    const x = Object.keys(data[0]).findIndex(key => key.toLowerCase().includes('sec'));
+    const xIndex = Object.keys(data[0]).findIndex(key => key.toLowerCase().includes('sec'));
+    const x = xIndex ? xIndex : 0;
     return {
       data,
-      x: x ? x : 0,
-      y: 1
+      x,
+      y: 1,
+      xText: data[0][x]
     };
   }
   handleKeyDown = (e) => {
     if (e.key === 'ArrowDown') {
       const { y, data } = this.state;
       const newY = (y + 1 >= data.length - 1) ? data.length - 1 : y + 1;
-      this.setState({ y: newY })
+      this.setState({ y: newY, yText: [Object.keys(data[0])[newY]] })
     }
     if (e.key === 'ArrowUp') {
-      const { y } = this.state;
+      const { y, data } = this.state;
       const newY = y === 0 ? 0 : y - 1;
-      this.setState({ y: newY })
+      this.setState({ y: newY,  yText: [Object.keys(data[0])[newY]] })
     }
   }
   componentWillUnmount() {
@@ -42,8 +43,8 @@ class TempContainer extends Component {
   }
 
   render() {
-    const { data, x, y, xLine, yLines } = this.state;
-    return <LineGraph data={data} x={x} y={y} xLine={xLine} yLines={yLines} />
+    const { data, x, y, xText, yText } = this.state;
+    return <LineGraph data={data} x={x} y={y} xText={xText} yText={yText} />
   }
 }
 
